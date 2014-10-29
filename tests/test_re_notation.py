88589937fa961e98049210f141e911bc6c449d74
Adrian Ortega <elfus0.1@gmail.com>
2014-10-29 12:52:20 -0600

    Added more test cases

--- tests/test_re_notation.py
+++ tests/test_re_notation.py
@@ -11,7 +11,18 @@ import unittest
 class TestReNotation(unittest.TestCase):
     def test_infix_to_postfix(self):
         self.assertEqual("+34", infix_to_postfix("3+4"))
+        self.assertEqual("+a+bc", infix_to_postfix("a+b+c"))
+        self.assertEqual("+a+b+cd", infix_to_postfix("a+b+c+d"))
         self.assertEqual("++345", infix_to_postfix("(3+4)+5"))
+        self.assertEqual("++34+5b", infix_to_postfix("(3+4)+(5+b)"))
+        self.assertEqual("+a++34+5b", infix_to_postfix("a+(3+4)+(5+b)"))
+        self.assertEqual("+a++34++5bc", infix_to_postfix("a+(3+4)+(5+b)+c"))
+        self.assertEqual("+a+x++34+5b", infix_to_postfix("a+x+(3+4)+(5+b)"))
+        self.assertEqual("+a+x++34++5b+cy", infix_to_postfix("a+x+(3+4)+(5+b)+c+y"))
+        self.assertEqual("++34++5bc", infix_to_postfix("(3+4)+(5+b)+c"))
+        self.assertEqual("++34++5b+cy", infix_to_postfix("(3+4)+(5+b)+c+y"))
+        self.assertEqual("+++34++5bcy", infix_to_postfix("((3+4)+(5+b)+c)+y"))
+        self.assertEqual("+a+x+++34++5bcy", infix_to_postfix("a+x+((3+4)+(5+b)+c)+y"))
         self.assertEqual("++ab+cd", infix_to_postfix("(a+b)+(c+d)"))
         self.assertEqual("*a", infix_to_postfix("a*"))
         self.assertEqual("*a*b", infix_to_postfix("a*b*"))
